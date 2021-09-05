87159dfb74ae984bd08568bde553a4211edb7eb4
Leonardo Asitimbaya <leonardoasitimbaya@gmail.com>
2021-09-05 14:20:24 -0500

    "Renombrado Clases"

--- src/main/java/rutaMinima/Controller.java
+++ src/main/java/rutaMinima/Controller.java
@@ -3,7 +3,7 @@ package rutaMinima;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
